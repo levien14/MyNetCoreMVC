8e495748a1fcf0d37df1015f80e0c0977b163a9f
LeVien <vienlvv14091998@gmail.com>
2018-10-29 14:32:34 +0700

    first commit

---
+++ MyNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MyNetCoreMVC.Models;
+
+namespace MyNetCoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetList()
+        {
+            return View();
+        }
+
+        //[Route("Product/Create/{name:string}")]
+        public IActionResult Create(string name, string price)
+        {
+            ViewData["Name"] = name;
+            ViewData["Price"] = price;
+
+            
+
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+
+           
+        }
+        [Route("Product/Delete/{id:int}")]
+        public IActionResult Delete(int Id)
+        {
+            return new JsonResult(new Product {
+              id = Id
+             
+            });
+        }
+    }
+}
